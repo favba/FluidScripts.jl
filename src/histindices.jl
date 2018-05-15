c92bed8332a9bf5cf8aa7dc1b8dc6895e22a3c2e
Felipe A. V. de Bragança Alves <favbalves@gmail.com>
2018-05-15 17:09:27 -0400

    Save nbins on file name.

--- src/histindices.jl
+++ src/histindices.jl
@@ -5,5 +5,5 @@ function histindices(input::AbstractString,nbins::Integer)
     minf,maxf = min_max(field)
     ind = hist_indices(field,minf,maxf,nbins)
     bins = Bins(minf,maxf,nbins)
-    @save "pdf_indices_$(input).jld2" ind bins
+    @save "pdf_indices_$(input)_nbins$(nbins).jld2" ind bins
 end
