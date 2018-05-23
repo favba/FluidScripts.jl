a6882b661a2c70eb1d6d352e9d5a50e5bd6ffc43
Felipe A. V. de Bragança Alves <favbalves@gmail.com>
2018-05-23 17:49:29 -0400

    Updated condmean.

--- test/condmean.jl
+++ test/condmean.jl
@@ -1,15 +1,26 @@
 #!/usr/bin/env julia
 using MyStats.condmean
-using ReadGlobal.readfield
+using ReadGlobal: readfield, readfield!
 using JLD2
 
-function main(inpname::AbstractString, condname::AbstractString, outfilename::AbstractString)
-    field = readfield(inpname)
+function main(condname::AbstractString, inpname::AbstractVector{<:AbstractString}, outfilename::AbstractVector{AbstractString})
     @load condname ind bins
-    r = condmean(field,ind)
-    writedlm(outfilename,hcat(bins,r))
+    field = readfield(inpname[1])
+    for i in linearindices(inpname)
+        readfield!(inpname[i],field)
+        r = condmean(field,ind)
+        writedlm(outfilename[i],hcat(bins,r))
+    end
     return nothing
 end
 
+function main(args::Vector{<:AbstractString})
+    condname = args[1]
+    iseven(length(args)) && error()
+    p = div(length(args) - 1, 2)
+    inpname = args[2:p]
+    outfilename = args[p+1:end]
+    main(condname,inpname,outfilename)
+end
 
-main(ARGS...)
+main(ARGS)
